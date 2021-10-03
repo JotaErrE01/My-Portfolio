0e0e821026f2f25e71efbf726204bf6ef5728f44
Jonathan Ruiz <jruiz.967@gmail.com>
2021-10-02 20:21:00 -0500

    CSS y HTML finalizado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
 import "../public/index.html";
 import './styles/styles.scss';
 
-console.log("hello world");
+// Selectors
+const $mainNav = document.querySelector('.nav-styles');
+const $titleAboutMe = document.querySelector('.aboutMe__title');
+const $aboutMeContainer = document.querySelector('#about_me');
+const $hamburguerMenu = document.querySelector('.menu');
+const $modalMenu = document.querySelector('.header__modal');
+const $modalClose = document.querySelector('.modal__close');
+const $mobileNavs = document.querySelectorAll('#nav__mobile');
+
+const observer = new IntersectionObserver( entries => {
+
+    const [ entrie ] = entries;
+
+    if( entrie.isIntersecting ){
+        $mainNav.classList.remove( 'navAnimation' );
+        $aboutMeContainer.classList.remove( 'mt-10' );
+    }else{
+        $mainNav.classList.add( 'navAnimation' );
+        $aboutMeContainer.classList.add( 'mt-10' );
+    }
+
+});
+
+observer.observe($titleAboutMe);
+
+// EVENTS
+$hamburguerMenu.addEventListener( 'click', () => { 
+   $modalMenu.classList.remove( 'd-none' ); 
+});
+
+$modalClose.addEventListener( 'click', () => {    
+    $modalMenu.classList.add( 'd-none' );
+});
+
+$mobileNavs.forEach( nav => { 
+    nav.addEventListener( 'click', () => {
+        $modalMenu.classList.add( 'd-none' );
+    } );
+});
+
+
+// const $btn = document.querySelector('.btn');
+
+// $btn.addEventListener('click', console.log );
